c1f0bfbce574e0e603b5b7652164c1337928989a
xiaohr <xiaohaoru6@163.com>
2020-07-06 20:17:34 +0800

    Initial commit

---
+++ first.py
@@ -0,0 +1,10 @@
+import random
+section1 = random.randint(1,254)
+section2 = random.randint(1,254)
+section3 = random.randint(1,254)
+section4 = random.randint(1,254)
+random_ip = str(section1) + '.' + str(section2) + '.' + str(section3) + '.' + str(section4)
+print(random_ip)
+
+
+
